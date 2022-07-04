e3af426917b84b5e21e96f947b32f945af080098
Logan Lampton <lampton.logan@gmail.com>
2022-07-04 14:30:55 -0400

    commit tree 12f6f4256ab73026034b6181887d1a85467ba1bc

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+} 
+name = 'Ralph';
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+name = 'Bob';
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, 'Broom'];
+} 
+
+function prependCat(name) {
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat() {
+    return [...cats.slice(0, cats.length -1)];
+}
+
+function removeFirstCat() {
+    return [...cats.slice(1)];
+}
